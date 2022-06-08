588115b2de9e5feaa3f46ade01969e0182072b18
Stephen Fabian <fabianstephen@gmail.com>
2022-06-08 11:09:22 -0600

    Run methods on existing arrays

--- arrays.rb
+++ arrays.rb
@@ -1,7 +1,15 @@
 friends = ["Cam", "Ethan", "Max", "Nicole"]
+# push method - will push the given element to the end of the given array, and will return the array, including the pushed elements.
+friends.push("Tim")
 
 integers = [1, 2, 3, 4]
+# shift method - will remove the first element of the array and return it
+integers.shift
 
 decimals_floats = [1.1, 2.2, 3.3, 4.4]
+# unshift method - will take the given elements and fill them at the front of the array, and will return array including the element(s) we added to the front of array
+decimals_floats.unshift(9.9)
 
 boooooleans = [TRUE, FALSE, TRUE, FALSE]
+# pop method - will remove the last element of the array and return the removed elements
+boooooleans.pop
